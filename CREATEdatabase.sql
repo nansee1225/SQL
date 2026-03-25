28059540b3f6cc7d8af4ae65b36be8578dcf65a0
Nansee Gupta <ng7905@srmist.edu.in>
2026-03-25 20:27:35 +0530

    h

--- CREATEdatabase.sql
+++ CREATEdatabase.sql
@@ -1,11 +1,14 @@
 create database IF NOT EXISTS collegeDB;
 use collegeDB;
-create table students
-use table students(
+create table students(
     STUDENTID int auto_increment primary key,
     NAME VARCHAR(100),
     EMAIL VARCHAR (100),
-    PHONEn0 INT,
+    PHONEno INT,
     JOINdate DATE
 );
-SELECT * FROM STUDENTS
+insert into students(STUDENTID NAME EMAIL PHONEno JOINdate )
+values('ISHA SINGH','ISHA8SINGH@gmail.com',4536829,'2025-06-21');
+
+
+select * from students;
