c1b1f36023d7d867c598eec98b74268f6635b45c
Nansee Gupta <ng7905@srmist.edu.in>
2026-03-29 14:39:55 +0530

    h

--- alter.sql
+++ alter.sql
@@ -1,5 +1,16 @@
+DROP TABLE IF EXISTS students;
+DROP DATABASE IF EXISTS collegedb;
 CREATE DATABASE  collegedb;
-USE DATABASE collegedb;
+USE  collegedb;
+
 create table students(
-studentid int auto
+studentid int auto_increment primary key,
+NAME varchar(100),
+age int,
+email varchar(200)
 );
+insert into students(NAME , age ,email )
+values
+('pooja SINGH',34,'pooja4singh@gmail.com'),
+('radhika singh', 36, 'radhika@gmail.com');
+select * from students;
