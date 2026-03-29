c72a98f79ee4e2233b132faefd5a8c9cdda00b9b
Nansee Gupta <ng7905@srmist.edu.in>
2026-03-29 23:09:47 +0530

    h

--- alter.sql
+++ alter.sql
@@ -13,4 +13,8 @@ insert into students(NAME , age ,email )
 values
 ('pooja SINGH',34,'pooja4singh@gmail.com'),
 ('radhika singh', 36, 'radhika@gmail.com');
+alter table students add column city varchar(100);
+DELETE FROM students WHERE NAME IS NULL;
+update students set city='modinagr' where studentid =1;
+update students set city='gzb' where studentid =2;
 select * from students;
